a71c2813fbb62408ec1f94f2c9e9a94d1d15d749
Kyle Husmann <kyle.husmann@gmail.com>
2022-06-10 13:29:00 -0400

    add better error

--- src/doit/study/io/__init__.py
+++ src/doit/study/io/__init__.py
@@ -15,8 +15,14 @@ def load_studyspec_str(
     instruments: t.Mapping[str, str],
     parser: t.Callable[[str], t.Any],
 ):
+    loaded_instruments: t.Mapping[str, InstrumentSpec] = {}
+    for k, v in instruments.items():
+        try:
+            loaded_instruments |= { k: InstrumentSpec.parse_obj(parser(v)) }
+        except Exception as e:
+            raise Exception("Error parsing {}; {}".format(k, e))
     return StudySpec(
         config=StudyConfigSpec.parse_obj(parser(config)),
         measures={ k: MeasureSpec.parse_obj(parser(v)) for k, v in measures.items() },
-        instruments={ k: InstrumentSpec.parse_obj(parser(v)) for k, v in instruments.items() },
+        instruments=loaded_instruments,
     )
